f1b166d58f3cceeba8fa2ab98f88dbe34b2ae3a6
Antonio Cappiello <antoniocappiello1@gmail.com>
2015-11-08 21:08:56 +0100

    Added the first example app to show how to import and use the library.

--- app/src/main/java/com/antoniocappiello/dateutilsexample/ExampleActivity.java
+++ app/src/main/java/com/antoniocappiello/dateutilsexample/ExampleActivity.java
@@ -1,7 +1,12 @@
 package com.antoniocappiello.dateutilsexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.antoniocappiello.dateutils.DateUtils;
+
+import java.util.Date;
 
 public class ExampleActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class ExampleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_example);
+
+        TextView examplesView = (TextView) findViewById(R.id.examples);
+        String time = DateUtils.getTime(new Date(System.currentTimeMillis()));
+
+        examplesView.setText(" + DateUtils.getTime(DATE) -> " + time);
+        examplesView.append("\n + more utils will follow with...");
+        examplesView.append("\n + ...other blog posts.");
+
     }
 }
